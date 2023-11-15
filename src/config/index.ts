e9506a34994adcc831b31ef59c06c01cccdb2ce9
Muhammad Haris Moin <hmoin@nisum.com>
2023-11-15 12:58:08 +0500

    code refactored

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 module.exports = {
   port: 4000,
   mongooseConfig: {
-    url: `mongodb+srv://harismoin:evEPbzfDaPwFCz0N@cluster0.hgcar.mongodb.net/blogpost?retryWrites=true&w=majority`,
+    url: `mongoURL`,
   },
   jwtConfig: {
-    secret: "iu12erQWu6sAVPLnJyy2pCXfasuperGPeAsThtkeyG3gdasdaq12eriu",
+    secret: "seceret key",
   },
 };
